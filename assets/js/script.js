8aff453eb813f951e1d3c77a61b53b067258e822
Proggery <80548375+Proggery@users.noreply.github.com>
2023-05-22 16:06:29 +0200

    halb fertig

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
 // @ts-nocheck
-const searchBtn = document.getElementById("searchBtn");
+const searchBtn = document.querySelector(".searchBtn");
+const searchInput = document.querySelector(".searchInput");
+const searchCloseBtn = document.querySelector(".searchCloseBtn");
 
 searchBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  searchBtn.classList.add = "teszt";
+    searchInput.classList.remove("form-hidden");
+    searchInput.classList.add("form-visible");
+    searchBtn.classList.add("form-hidden");
+    searchInput.style.transition = "2s";
+});
+
+searchCloseBtn.addEventListener("click", (e) => {
+    searchInput.classList.add("form-hidden");
+    searchInput.classList.remove("form-visible");
+    searchBtn.classList.add("form-visible");
+    // window.location("http://127.0.0.1:5500/index.html")
+});
+
+//--- SLIDER ---
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: false,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 });
