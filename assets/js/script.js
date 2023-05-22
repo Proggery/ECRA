c61937975d4d56aa2a297602ab03fd97aab28710
Proggery <80548375+Proggery@users.noreply.github.com>
2023-05-22 12:19:02 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// @ts-nocheck
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchBtn.classList.add = "teszt";
+});
