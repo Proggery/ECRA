3698cbe115af41ff9038357f19c04c096a4882e0
Proggery <80548375+Proggery@users.noreply.github.com>
2023-05-24 14:36:03 +0200

    carousel finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,49 @@ const searchCloseBtn = document.querySelector(".searchCloseBtn");
 const searchBG = document.querySelector(".searchBG");
 
 searchBtn.addEventListener("click", (e) => {
-    searchInput.classList.remove("form-hidden");
-    searchInput.classList.add("form-visible");
-    searchBtn.classList.remove("form-visible");
-    searchBtn.classList.add("form-hidden");
+  searchInput.classList.remove("form-hidden");
+  searchInput.classList.add("form-visible");
+  searchBtn.classList.remove("form-visible");
+  searchBtn.classList.add("form-hidden");
 });
 
 searchCloseBtn.addEventListener("click", (e) => {
-    searchInput.classList.add("form-hidden");
-    searchInput.classList.remove("form-visible");
-    searchBtn.classList.add("form-visible");
-    // window.location("http://127.0.0.1:5500/index.html")
+  searchInput.classList.add("form-hidden");
+  searchInput.classList.remove("form-visible");
+  searchBtn.classList.add("form-visible");
 });
-
-// searchBG.children.setAttribute("teszt", "bla");
-// searchBG.children.forEach(element => {
-//     console.log(element);
-// });
-
 
 //--- SLIDER ---
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-    grabCursor: true,
-    pagination: false,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+  slidesPerView: 5,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: false,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 });
+
+//--- CAROUSEL ---
+const mediaQuery = window.matchMedia("(max-width: 767.9px)");
+const carouselID = document.querySelector("#carouselExample");
+
+const handleTabletChange = (e) => {
+    if (e.matches) {
+      carouselID.id = "carouselExampleInterval"
+      carouselID.setAttribute("data-bs-ride","carousel")
+    } else {
+      carouselID.id = "carouselExample"
+      carouselID.removeAttribute("data-bs-ride","carousel")
+    }
+}
+mediaQuery.addListener(handleTabletChange)
+handleTabletChange(mediaQuery)
+
+//id
+// carouselExampleInterval;
+// data-bs-ride="carousel"
+
+// button
+// data-bs-target="#carouselExampleInterval";
